683b17c8f2b1f60ff659b62f76f7fce8e0750f81
Lukas <lukash2010@web.de>
2026-03-13 16:25:35 +0100

    Remove test files and unused configurations

--- frontend/src/fix_jsx.ps1
+++
@@ -1,3 +0,0 @@
-$content = Get-Content "App_clean.jsx"
-$fixed = $content -replace "            </div>`r`n          </div>`r`n        </div>`r`n      </div>`r`n    </div>`r`n  );", "            </div>`r`n          </div>`r`n        </div>`r`n      </div>`r`n    </div>`r`n          </div>`r`n  );"
-Set-Content "App_clean_fixed.jsx" -Value $fixed
