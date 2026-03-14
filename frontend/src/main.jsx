83d1c2a17cc8cfdfab6a1067e7e9a99db6cd0e15
Lukas <lukash2010@web.de>
2026-03-14 11:04:02 +0100

    Fix frontend build - correct import path in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App_final_fixed from './App_final_fixed.jsx'
+import App_final from './App_final.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App_final_fixed />
+    <App_final />
   </React.StrictMode>,
 )
